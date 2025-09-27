425d75b3d88c027f05badd1ddcfd6fd727ad3a5f
wenqingl35 <wenqingl@nycstudents.net>
2025-09-27 19:46:17 +0000

    dfsdf

--- src/main/java/com/example/TipCalculator.java
+++ src/main/java/com/example/TipCalculator.java
@@ -5,17 +5,19 @@ public class TipCalculator {
     //WRITE YOUR PROGRAM IN calculateTip
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
-        double totalTip = Math.round((cost * percent)) / 100.0;
-        double totalBill = Math.round((cost + totalTip) * 100.0) / 100.0;
+        double totalTip = (cost * percent) / 100;
+        double totalBill = (cost + totalTip) * 100.0 / 100.0;
         //I learned how to read from https://stackoverflow.com/questions/8825209/rounding-decimal-points
-        double costBperson = Math.round((cost / people) * 100.0) / 100.0;
-        double tipPerson = Math.round((totalTip / people) * 100.0) / 100.0;
-        double costAperon = Math.round((totalBill / people) * 100.0) / 100.0;
+        double costBperson = Math.round(((cost / people) * 100.0)) / 100.0;
+        double tipPerson = Math.round(((totalTip / people) * 100.0)) / 100.0;
+        double costAperon = Math.round(((totalBill / people) * 100.0)) / 100.0;
+        double roundedTotaltip = (Math.round((totalTip * 100))) / 100.0;
+        double roundedTotalbill = (Math.round((totalBill * 100))) / 100.0;
         String result = "-------------------------------\n" +
                        "Total bill before tip: $" + cost + "\n" +
                        "Total percentage: " + percent + "%\n" +
-                       "Total tip: $" + totalTip + "\n" +
-                       "Total Bill with tip: $" + totalBill + "\n" +
+                       "Total tip: $" + roundedTotaltip + "\n" +
+                       "Total Bill with tip: $" + roundedTotalbill + "\n" +
                        "Per person cost before tip: $" + costBperson + "\n" +
                        "Tip per person: $" + tipPerson + "\n" +
                        "Total cost per person: $" + costAperon + "\n" +
@@ -50,9 +52,9 @@ public class TipCalculator {
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
         //try different values for people, percent, and cost to test your program before running test cases
-        int people = 3;
-        int percent = 30;
-        double cost = 75.50;          
+        int people = 12;
+        int percent = 15;
+        double cost = 566.97;        
         System.out.println(calculateTip(people,percent,cost));
         System.out.println(extraCredit(people, percent, cost));
     }
